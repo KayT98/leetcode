fa9b7a1fb4104c03985559d65c0edbeb2eadca90
KayT98 <khanhtruong109@yahoo.com>
2023-10-28 20:58:44 -0500

    added comments

--- Sleep.js
+++ Sleep.js
@@ -4,3 +4,6 @@
 async function sleep(millis) {
     await new Promise(resolve => {setTimeout(resolve, millis)})
 }
+/**defines an asynchronous function sleep that introduces a delay by waiting for a 
+specified number of milliseconds using a Promise and setTimeout. It pauses the execution for the given millis duration.
+**/
--- TwoSum.js
+++ TwoSum.js
@@ -1,13 +1,13 @@
 /**
  * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * @param {number} target //these are LC defaults to describe parameters for the function
+ * @return {number[]} //return array of numbers
  */
 let twoSum = function(nums, target) {
-    for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j<nums.length; j++){
-            if(nums[i] + nums[j] == target){
-                return [i,j];
+    for(let i = 0; i < nums.length; i++){//iterates through elements in the nums array using the index i
+        for(let j = i+1; j<nums.length; j++){ //iterates through elements following the current i in the nums array using the index j
+            if(nums[i] + nums[j] == target){ //check if the sums equal to target
+                return [i,j]; //if the condition is met, returns an array containing the indices i, j as a pair.
             }
         }
     }
